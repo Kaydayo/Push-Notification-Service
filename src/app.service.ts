f5ce678d05946bec2e4b10529c2ec7bc16cb8a99
Mujeeb Adebowale <olukayodedayo100@gmail.com>
2022-09-19 20:38:54 +0100

    sendAll firebase

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import firebaseCred from '../testKey.json';
 import * as firebase from 'firebase-admin'
+import { BatchResponse } from 'firebase-admin/lib/messaging/messaging-api';
+import * as shell from 'shelljs'
 
 @Injectable()
 export class AppService {
@@ -13,29 +15,17 @@ export class AppService {
     })
   }
 
-  
+  public async sendAll(messages: firebase.messaging.TokenMessage[], dryRun?: boolean): Promise<BatchResponse>{
+    if (process.env.NODE_ENV === 'local') {
+      for (const { notification, token } of messages) {
+        shell.exec(
+          `echo '{ "aps": { "alert": ${JSON.stringify(notification)}, "token": "${token}" } }' | xcrun simctl push booted com.company.appname -`
+        )
+      }
+    }
+    return firebase.messaging().sendAll(messages, dryRun)
+  }
   
 }
 
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD8shq5l87XOy4kXs-aBkXmej0-B5kKWwo",
-  authDomain: "push-notification-f220f.firebaseapp.com",
-  projectId: "push-notification-f220f",
-  storageBucket: "push-notification-f220f.appspot.com",
-  messagingSenderId: "1057503803906",
-  appId: "1:1057503803906:web:eef4b973b521d735b0851b",
-  measurementId: "G-D3BSB7S8D3"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
